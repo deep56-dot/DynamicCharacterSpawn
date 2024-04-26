da9a5a510d254a18864afc787ef1ff6a043214f1
Deep Dhuvad <deepdhuvad68@gmail.com>
2024-04-26 15:41:39 +0530

    assignment 2 completed

---
+++ Source/Assignment2/Assignment2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Assignment2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSIGNMENT2_API AAssignment2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
